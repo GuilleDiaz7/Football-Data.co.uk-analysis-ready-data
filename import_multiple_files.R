cbb417ddfd65b6a295a8479ea1ca40988a397557
Guillermo Díaz <42537388+GuilleDiaz7@users.noreply.github.com>
2022-09-14 10:24:28 +0200

    Update import_multiple_files.R
    Uploading R script to import multiple .csv files.

--- import_multiple_files.R
+++ import_multiple_files.R
@@ -1,2 +1,33 @@
-## This script goal is to import multiple .csv files from a local folder using relative paths
-## I recommend learning how to use RStudio Projects 
+## This script's goal is to import multiple .csv files from a local folder using relative paths
+## I recommend learning how to use RStudio Projects first
+
+#### BASIC CODE ####
+## Clean data memory
+rm(list = ls())
+rm("what") # Clean a specific R object
+
+## Remove plots
+dev.off(dev.list()["RStudioGD"]) # Apply dev.off() & dev.list()
+
+
+#### LOAD PACKAGES ####
+library(dplyr) # Cleaning and processing data
+library(purrr) # Map function
+library(tidyr)
+library(plyr) # Function ldply
+library(stringr) # Extracting data from strings
+library(lubridate) # Parsing dates and times
+
+
+#### IMPORT DATA ####
+## Open multiple files
+file_paths <- fs::dir_ls("data/football-data.co.uk") # Note that I am using relative paths on local
+
+## Create a list of dataframes
+data <- file_paths %>% 
+  map(function (path) {
+    read.csv(path)
+  })
+
+## Convert list of dataframe into a single dataframe
+data <- ldply(data, data.frame)
